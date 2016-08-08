ed63a0e02925516c1830ad53121196ce4707ee16
bp <fzvt08@USAUSWNCN196722.nam.corp.gm.com>
2016-08-08 15:30:44 -0500

    started on ArtPage. need to add a footer

---
+++ app/components/art/ArtPage.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import React, { PropTypes } from "react";
+
+class ArtPage extends React.Component {
+    constructor(props, context) {
+      super(props, context);
+    }
+
+    render() {
+      return (
+        <div>
+        </div>
+      );
+    }
+}
+
+
+export default ArtPage;
